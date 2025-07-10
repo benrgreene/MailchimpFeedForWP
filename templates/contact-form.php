dcb87867a60e27e9e43ae21cc8f8155e17126c49
Ben <benrgreene@gmail.com>
2025-07-09 18:06:51 -0700

    update newletters to display by year

--- templates/contact-form.php
+++ templates/contact-form.php
@@ -3,31 +3,23 @@
 <div class="<?= $classes ?>">
 	<form class="form" ajax-form action="<?= site_url() ?>/wp-json/mailchimp/contact-submit" method="POST">
 		<div class="input-wrapper">
-			<label for="email">
-				Email *
-			</label>
+			<label for="email">Email *</label>
 			<input name="email" id="email" type="email" required="required" />
 		</div>
 
 		<div class="input-wrapper">
-			<label for="subject">
-				Subject *
-			</label>
+			<label for="subject">Subject *</label>
 			<input name="subject" id="subject" type="text" required="required" />
 		</div>
 
 		<div class="input-wrapper">
-			<label for="message">
-				Message *
-			</label>
+			<label for="message">Message *</label>
 			<textarea name="message" id="message" required="required"></textarea>
 		</div>
 
 		<div class="check-wrapper">
 			<input name="subscribe" id="subscribe" type="checkbox" />
-			<label for="subscribe">
-				Subscribe to our newsletter
-			</label>
+			<label for="subscribe">Subscribe to our newsletter</label>
 		</div>
 
 		<input type="submit" value="Send Message" class="button" />
